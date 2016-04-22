950594ff35d79bc729d289ef4aa1c0c7bace16ed
hwchung <h.w.chung27@gmail.com>
2016-04-21 21:10:23 -0400

    Added files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$(document).on('change', '.btn-file :file', function() {
+  var input = $(this),
+      numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+    });
+    
+    $('#saveAndExitBtn').click(function(e) {
+      localStorage.setItem("newPatient", "true");
+    });
+
+    $('#saveNewMeds').click(function (e) {
+      localStorage.setItem("newMeds", "true");
+    });
+});
